7826051c38776a5a05192c95435860a966bd2395
Евгений Шигаев <shigaev-evgeniy@yandex.ru>
2026-01-06 03:06:07 +0400

    поменял логин эндпоинт

--- back/src/main/java/com/example/coopProject/controller/AuthController.java
+++ back/src/main/java/com/example/coopProject/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class AuthController {
 
--- back/src/main/java/com/example/coopProject/security/SecurityConfig.java
+++ back/src/main/java/com/example/coopProject/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login", "api/users/register").permitAll()
+                        .requestMatchers("/api/users/login", "api/users/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
